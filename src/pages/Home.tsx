6d8e8a1d8391025880d87121327d9d899387a511
Ethan Zemanick <ethanzem@buffalo.edu>
2023-09-04 17:19:37 -0400

    Test push

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import '../styles/Home.css';
 export const Home = () => {
   return (
     <div className='page-container'>
-        This is our home page for Z-Audio
+      Welcome to Z-Audio
     </div>
 
   )
